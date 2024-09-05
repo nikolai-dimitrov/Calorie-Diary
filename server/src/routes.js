9188fafab4a8a75fa73339e10d694626e942676f
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-09-05 13:42:08 +0300

    server/feat: Create route matches invalid url

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
 const userController = require("./controllers/userController");
+const profileController = require("./controllers/profileController");
 
 router.use("/users", userController);
+router.use("/profiles", profileController);
+router.all("*", (req, res, next) => {
+	res.status(404).json({ message: `Page not found` });
+});
 
 module.exports = router;
