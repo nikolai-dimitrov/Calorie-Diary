e48db687be06fd4439cbb6966fef376abda5a6ac
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-09-06 16:38:31 +0300

    server/feat: Add get profile route and authRequired middleware

--- server/src/controllers/profileController.js
+++ server/src/controllers/profileController.js
@@ -1,11 +1,20 @@
 const router = require("express").Router();
 const profileService = require("../services/profileService");
+const { isAuthRequired } = require("../middlewares/authMiddleware");
 
-router.get("/", async (req, res, next) => {
+router.get("/", isAuthRequired(true), async (req, res, next) => {
+	try {
+		const userId = req.user?._id;
+		const profile = await profileService.getProfile(userId);
+		res.json(profile);
+	} catch (error) {
+		next(error);
+	}
+
 	// TODO:
 });
 
-router.post("/", async (req, res, next) => {
+router.post("/", isAuthRequired(true), async (req, res, next) => {
 	try {
 		const profileData = { ...req.body, owner: req.user?._id };
 		const profile = await profileService.createProfile(profileData);
@@ -14,5 +23,5 @@ router.post("/", async (req, res, next) => {
 		next(error);
 	}
 });
-
+router.put("/", isAuthRequired(true), async (req, res, next) => {});
 module.exports = router;
