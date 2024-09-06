01d2d71eb614a866c562342a814fbdb44fe98997
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-09-06 16:11:52 +0300

    server/feat: Add create profile post route

--- server/src/controllers/profileController.js
+++ server/src/controllers/profileController.js
@@ -1,7 +1,18 @@
 const router = require("express").Router();
+const profileService = require("../services/profileService");
 
 router.get("/", async (req, res, next) => {
-	res.json("Profile");
+	// TODO:
+});
+
+router.post("/", async (req, res, next) => {
+	try {
+		const profileData = { ...req.body, owner: req.user?._id };
+		const profile = await profileService.createProfile(profileData);
+		res.json(profile);
+	} catch (error) {
+		next(error);
+	}
 });
 
 module.exports = router;
