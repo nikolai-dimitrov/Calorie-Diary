5cf3fd39a2dadca56c683bb3136b1d76caea2a57
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-09-06 17:00:39 +0300

    server/refactor: Change response format

--- server/src/controllers/profileController.js
+++ server/src/controllers/profileController.js
@@ -1,27 +1,43 @@
 const router = require("express").Router();
 const profileService = require("../services/profileService");
 const { isAuthRequired } = require("../middlewares/authMiddleware");
-
+// Get profile
 router.get("/", isAuthRequired(true), async (req, res, next) => {
 	try {
 		const userId = req.user?._id;
 		const profile = await profileService.getProfile(userId);
-		res.json(profile);
+		res.status(200).json({
+			status: "success",
+			data: profile,
+		});
 	} catch (error) {
 		next(error);
 	}
-
-	// TODO:
 });
-
+// Create profile
 router.post("/", isAuthRequired(true), async (req, res, next) => {
 	try {
 		const profileData = { ...req.body, owner: req.user?._id };
 		const profile = await profileService.createProfile(profileData);
-		res.json(profile);
+		res.status(201).json({
+			status: "success",
+			data: profile,
+		});
 	} catch (error) {
 		next(error);
 	}
 });
-router.put("/", isAuthRequired(true), async (req, res, next) => {});
+// Update profile
+router.put("/", isAuthRequired(true), async (req, res, next) => {
+	try {
+		const profileData = { ...req.body, owner: req.user?._id };
+		const profile = await profileService.updateProfile(profileData);
+		res.status(200).json({
+			status: "success",
+			data: profile,
+		});
+	} catch (error) {
+		next(error);
+	}
+});
 module.exports = router;
