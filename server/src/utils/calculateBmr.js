7580b5e4645d3b497aa8d931acbad87a95fd1863
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-09-06 17:34:46 +0300

    server/refactor: Change variable names
    Function take args as object instead of separate args

--- server/src/utils/calculateBmr.js
+++ server/src/utils/calculateBmr.js
@@ -1,13 +1,13 @@
-exports.calculateBmr = (weight, height, age, gender) => {
-	console.log(weight, height, age);
+// Calculate bmr based on provided user information
+exports.calculateBmr = ({ currentWeight, height, age, gender }) => {
 	const bmr =
 		gender == "male"
 			? 88.362 +
-			  13.397 * Number(weight) +
+			  13.397 * Number(currentWeight) +
 			  4.799 * Number(height) -
 			  5.677 * Number(age)
 			: 447.593 +
-			  9.247 * Number(weight) +
+			  9.247 * Number(currentWeight) +
 			  3.098 * Number(height) -
 			  4.33 * Number(age);
 	return bmr;
