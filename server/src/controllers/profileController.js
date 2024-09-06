8ce1a0d5562b8e1b6322e8c8c6a830f7d10ee095
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-09-06 19:13:29 +0300

    server/docs: Add profile controller comments

---
+++ server/src/controllers/profileController.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const profileService = require("../services/profileService");
+const { isAuthRequired } = require("../middlewares/authMiddleware");
+// Get profile
+router.get("/", isAuthRequired(true), async (req, res, next) => {
+	try {
+		const userId = req.user?._id;
+		const profile = await profileService.getProfile(userId);
+		res.status(200).json({
+			status: "success",
+			data: profile,
+		});
+	} catch (error) {
+		next(error);
+	}
+}); 
+
+// Create profile
+router.post("/", isAuthRequired(true), async (req, res, next) => {
+	try {
+		const profileData = { ...req.body, owner: req.user?._id };
+		const profile = await profileService.createProfile(profileData);
+		res.status(201).json({
+			status: "success",
+			data: profile,
+		});
+	} catch (error) {
+		next(error);
+	}
+});
+
+// Update profile
+router.put("/", isAuthRequired(true), async (req, res, next) => {
+	try {
+		const newProfileData = { ...req.body, owner: req.user?._id };
+		const profile = await profileService.updateProfile(newProfileData);
+		res.status(200).json({
+			status: "success",
+			data: profile,
+		});
+	} catch (error) {
+		next(error);
+	}
+});
+
+module.exports = router;
