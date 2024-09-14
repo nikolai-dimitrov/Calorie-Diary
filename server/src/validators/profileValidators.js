2acbf90a38e145a7d9b6f34cd4c943032c94ccf4
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-09-14 23:20:26 +0300

    server/refactor: Remove if equals check in profile validator

--- server/src/validators/profileValidators.js
+++ server/src/validators/profileValidators.js
@@ -3,16 +3,17 @@ exports.validateWeightFields = (profileData) => {
 	let { currentWeight, targetWeight, bodyGoal } = profileData;
 	currentWeight = Number(currentWeight);
 	targetWeight = Number(targetWeight);
-	if (bodyGoal == "Lose Weight" && targetWeight >= currentWeight) {
+	if (bodyGoal == "Lose Weight" && targetWeight > currentWeight) {
 		throw new CustomError(
 			409,
 			"Your target weight should be less than the current weight"
 		);
-	} else if ((bodyGoal == "Gain Weight") & (targetWeight <= currentWeight)) {
+	} else if ((bodyGoal == "Gain Weight") & (targetWeight < currentWeight)) {
 		throw new CustomError(
 			409,
 			"Your target weight should be more than the current weight"
 		);
+		// TODO: AKO STANE POD ILI NAD DA MY DADE NOTIFIKACIQ CHE GUBI ILI KACHVA TEGLO
 	} else if (bodyGoal == "Maintain Weight" && targetWeight != currentWeight) {
 		throw new CustomError(
 			409,
