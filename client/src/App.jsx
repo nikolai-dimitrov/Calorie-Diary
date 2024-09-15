b87e5753df0bb490954c700000d82323f7971c81
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-09-15 16:07:47 +0300

    client/feat: Add navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Navbar } from './components/Navbar/Navbar';
+import { Login } from './components/Login/Login';
+import { UserProfile } from './components/UserProfile/UserProfile';
+
 import './App.css'
 function App() {
-
-  return (
-    <>
-    <div>Welcome</div>
-    </>
-  )
+	return (
+		<div className='main'>
+			<Navbar />
+			{/* <UserProfile /> */}
+			{/* <Login /> */}
+		</div>
+	)
 }
 
 export default App
