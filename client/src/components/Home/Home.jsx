109542dfa59ed9a07a61f5b50f58ddc542a43e79
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-09-16 12:47:14 +0300

    client/style: Update Home.jsx

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,5 +1,3 @@
-// Picture with some text and good looking page with weather temperature maybe
-// lenta s 5 6 neshta da minavat
 export const Home = () => {
       return (
             <div>Home</div>
