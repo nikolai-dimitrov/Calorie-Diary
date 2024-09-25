d651ef8f722c70c3a7b005e9e6f339b1886c678d
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-09-25 17:54:59 +0300

    client/feat: Create register page with functionalities

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,13 +1,39 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 import { AuthForm } from '../Common/AuthForm/AuthForm';
+import { useForm } from '../../hooks/useForm';
+import { useAuthStore } from '../../stores/authStore'
+import { validateAuth } from '../../utils/validateAuth';
 
 import styles from './register.module.css';
 
 export const Register = () => {
+      const { register, serverError } = useAuthStore()
+      const navigate = useNavigate()
+      const { formValues, formErrors, onChange, onSubmit, success, validateInput } = useForm({
+            email: '',
+            password: '',
+            repeatPassword: '',
+      }, register, validateAuth);
+
+      useEffect(() => {
+            if (success == true) {
+                  navigate('/')
+            }
+      }, [success])
+
       return (
             <section className={styles.register}>
                   <div className={styles.backgroundImg}></div>
                   <div className={styles.overlay}>
-                        <AuthForm />
+                        <AuthForm
+                              formValues={formValues}
+                              onSubmit={onSubmit}
+                              onChange={onChange}
+                              formErrors={formErrors}
+                              serverError={serverError}
+                              validateInput={validateInput}
+                        />
                   </div>
             </section>
       )
