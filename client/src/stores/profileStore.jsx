63cb4afc3a3a8710baada650e43de16af34c040f
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-01-18 14:39:27 +0200

    client/feat: Implement zustand profile store with create profile functionality

---
+++ client/src/stores/profileStore.jsx
@@ -0,0 +1,29 @@
+import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+import { profileServiceFactory } from '../services/profileService'
+
+export const useProfileStore = create(
+    devtools(
+        (set) => {
+            return (
+                {
+                    profile: null,
+                    serverError: '',
+                    createProfile: async (profileData) => {
+                        try {
+                            const profileService = profileServiceFactory();
+                            const result = await profileService.createProfile(profileData);
+                            set({ profile: result })
+                        } catch (error) {
+                            set((state) => ({ ...state, serverError: error.message }));
+
+                        }
+                    },
+
+                    clearServerErrors: () => {
+                        set({ serverError: null });
+                    }
+                })
+        },
+    )
+)
